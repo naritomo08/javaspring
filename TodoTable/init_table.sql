b15990e9e3e08d3a9960fbc8dd21011379b0a535
naritomo08 <naritomo08@gmail.com>
2024-03-24 17:47:18 +0900

    upload

--- TodoTable/init_table.sql
+++ TodoTable/init_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tododb
+CREATE TABLE todo
 (
     id          SERIAL PRIMARY KEY,
     title       TEXT,
@@ -6,13 +6,13 @@ CREATE TABLE tododb
     urgency     INTEGER,
     deadline    DATE,
     done        TEXT
-)
+);
 
-INSERT INTO todo(title,importance,urgency,deadline,done)
+INSERT INTO todo(title,importance,urgency,deadline,done) 
 VALUES('toso-1',0,0,'2020-10-01','N');
-INSERT INTO todo(title,importance,urgency,deadline,done)
+INSERT INTO todo(title,importance,urgency,deadline,done) 
 VALUES('toso-2',0,1,'2020-10-02','N');
-INSERT INTO todo(title,importance,urgency,deadline,done)
+INSERT INTO todo(title,importance,urgency,deadline,done) 
 VALUES('toso-3',1,0,'2020-10-03','N');
-INSERT INTO todo(title,importance,urgency,deadline,done)
+INSERT INTO todo(title,importance,urgency,deadline,done) 
 VALUES('toso-4',1,1,'2020-10-04','N');
