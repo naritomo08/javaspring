ea115167000747acb87f9d814c6c335f4d3bd749
naritomo08 <naritomo08@gmail.com>
2024-03-24 18:35:58 +0900

    upload

--- TodoTable/init_table.sql
+++ TodoTable/init_table.sql
@@ -9,10 +9,10 @@ CREATE TABLE todo
 );
 
 INSERT INTO todo(title,importance,urgency,deadline,done) 
-VALUES('toso-1',0,0,'2020-10-01','N');
+VALUES('todo-1',0,0,'2020-10-01','N');
 INSERT INTO todo(title,importance,urgency,deadline,done) 
-VALUES('toso-2',0,1,'2020-10-02','N');
+VALUES('todo-2',0,1,'2020-10-02','N');
 INSERT INTO todo(title,importance,urgency,deadline,done) 
-VALUES('toso-3',1,0,'2020-10-03','N');
+VALUES('todo-3',1,0,'2020-10-03','N');
 INSERT INTO todo(title,importance,urgency,deadline,done) 
-VALUES('toso-4',1,1,'2020-10-04','N');
+VALUES('todo-4',1,1,'2020-10-04','N');
