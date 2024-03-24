88dd896c47028c9d72f91ce1a9ad6201aaecbd9b
naritomo08 <naritomo08@gmail.com>
2024-03-24 14:16:11 +0900

    upload

--- Todolist/src/main/java/com/example/todolist/repository/TodoRepository.java
+++ Todolist/src/main/java/com/example/todolist/repository/TodoRepository.java
@@ -1,5 +1,9 @@
 package com.example.todolist.repository;
 
-public interface TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.todolist.entity.Todo;
 
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
 }
